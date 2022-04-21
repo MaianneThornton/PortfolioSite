72fc6409de057aab95b7c26aa1bcf77145ee8dc3
Maianne Thornton <maianne.thornton@gmail.com>
2022-04-21 12:31:33 -0400

    feat: personalized browser icon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//Get the button:
+//Get the return button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the return button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
@@ -12,7 +12,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the return button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
