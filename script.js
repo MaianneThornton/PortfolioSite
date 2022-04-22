d5242656d311075a499105e3ee841bb61f7e4dcb
Maianne Thornton <maianne.thornton@gmail.com>
2022-04-22 12:11:00 -0400

    feat: initial nav bar styles

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Get the return button:
 mybutton = document.getElementById("myBtn");
+// Get the nav button:
+navbtn = document.getElementById("nav-btn");
 
 // When the user scrolls down 20px from the top of the document, show the return button
 window.onscroll = function () { scrollFunction() };
@@ -16,4 +18,11 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function navToggle() {
+  navbtn.addEventListener('click', () => {
+    nav.classList.toggle('showLinks')
+    input.focus()
+  })
 }
